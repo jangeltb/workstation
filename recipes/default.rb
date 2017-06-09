ed20d00d1d09c53fef8bcf7f3621791db046c78a
J. Angel <jangel@mail.com>
2017-06-09 10:08:40 +0200

    Lanzamiento de receta desde default.rb

--- recipes/default.rb
+++ recipes/default.rb
@@ -3,3 +3,5 @@
 # Recipe:: default
 #
 # Copyright (c) 2017 The Authors, All Rights Reserved.
+# Se incluye la receta o recetas en el archivo default.rb para llamarlas y ejecutarlas todas desde un solo archivo
+include_recipe "workstation::setup"
