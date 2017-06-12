ae6117b75ed293708f4ae801059906bf047bcd57
J. Angel <jangel@mail.com>
2017-06-12 13:34:25 +0200

    Version 0.2.1: Uso de plantilla

--- recipes/setup.rb
+++ recipes/setup.rb
@@ -2,15 +2,9 @@ package "tree" do
 	action :install
 end
 
-file "/etc/motd" do
-	content "Property of ...
-
-	IPADDRESS: #{node['ipaddress']}
-	HOSTNAME: #{node['hostname']}
-	MEMORY: #{node['memory']['total']}
-	CPU: #{node['cpu']['0']['mhz']}
-"
-mode "0644"
-owner "root"
-group "root"
+template "/etc/motd" do
+  source 'motd.erb'
+  mode "0644"
+  owner "root"
+  group "root"
 end
